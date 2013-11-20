507a47f1fe3fc14fa0a6b2a3d5550313a25c8521
Amal Hussein <amalhussein08@gmail.com>
2013-11-20 16:48:30 -0500

    commit b4 merge

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 1.2'
 
-
 gem 'twitter' , '~> 5.0.0'
 # Caching starts here
 gem 'redis-store'
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,15 @@ class Product < ActiveRecord::Base
 	has_many :reviews
 
 	def tweets 
-		TwitterClient.user_timeline(self.twitter_user).map(&:text)
+			Rails.cache.fetch(["tweets", self.twitter_name], expires_in: 30.seconds) do 
+			TwitterClient.user_timeline(self.twitter_name).map(&:text)
+		end 
 	end 
-
 end
+
+
+
+
+
+
+
